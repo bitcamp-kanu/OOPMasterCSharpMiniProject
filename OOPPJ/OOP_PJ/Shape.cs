a63194037929bc068e4856d35a5fbb89429b88c1
BitCamp-PC\BitCamp <isolgwon@gmail.com>
2017-01-16 15:20:00 +0900

    삼각형 추가입니다

--- OOPPJ/OOP_PJ/Shape.cs
+++ OOPPJ/OOP_PJ/Shape.cs
@@ -14,8 +14,8 @@ namespace OOP_PJ
         private Color fillColor; // 도형 채우기 색
         private bool hasLine; // 라인 그리기 여부
         private bool hasFill; // 채우기 여부
-        private int sequenceNumber; // 상태 저장 순서 번호
-        
+        private int sequenceNumber; // 상태 자동 저장 순서 번호(신경 쓸필요없음)
+
         public bool HasFill { set { hasFill = value; } get { return hasFill; } }
         public bool HasLine { set { hasLine = value; } get { return hasLine; } }
         public int Thickness { set { thickness = value; } get { return thickness; } }
@@ -34,10 +34,10 @@ namespace OOP_PJ
 
     public class CCircle : Shape
     {
-        private Stack<CCircle> mHistory; 
+        private Stack<CCircle> mHistory;
         private Rectangle mRec;
         private int baseListIndex;
-        
+
         public CCircle(Rectangle recParam)
         {
             mHistory = new Stack<CCircle>();
@@ -55,7 +55,7 @@ namespace OOP_PJ
 
         public override int GetStackCount()
         {
-            return mHistory.Count;   
+            return mHistory.Count;
         }
 
         public override void Draw(Graphics g)
@@ -65,13 +65,13 @@ namespace OOP_PJ
                 SolidBrush brush = new SolidBrush(base.FillColor);
                 g.FillEllipse(brush, mRec);
             }
-            
-            if(base.HasLine)
+
+            if (base.HasLine)
             {
                 Pen pen = new Pen(base.LineColor, base.Thickness);
                 g.DrawEllipse(pen, mRec);
             }
-           
+
         }
 
         public override Rectangle GetRenctangle()
@@ -95,7 +95,7 @@ namespace OOP_PJ
             tmp.HasFill = this.HasFill;
             this.SequenceNumber++;
             tmp.SequenceNumber = this.SequenceNumber;
-            
+
 
             mHistory.Push(tmp);
         }
@@ -111,7 +111,7 @@ namespace OOP_PJ
                         return false;
                     tmp = mHistory.Pop();
                 }
-                 
+
                 this.mRec = tmp.mRec;
                 this.FillColor = tmp.FillColor;
                 this.Thickness = tmp.Thickness;
@@ -119,7 +119,7 @@ namespace OOP_PJ
                 this.HasLine = tmp.HasLine;
                 this.HasFill = tmp.HasFill;
                 this.ListIndex = tmp.ListIndex;
-                
+
                 return true;
             }
             return false;
@@ -167,12 +167,12 @@ namespace OOP_PJ
                 SolidBrush brush = new SolidBrush(base.FillColor);
                 g.FillRectangle(brush, mRec);
             }
-            if(base.HasLine)
+            if (base.HasLine)
             {
                 Pen pen = new Pen(base.LineColor, base.Thickness);
                 g.DrawRectangle(pen, mRec);
             }
-          
+
         }
 
         public override Rectangle GetRenctangle()
@@ -206,6 +206,112 @@ namespace OOP_PJ
             if (mHistory.Count > 0)
             {
                 CRectangle tmp = mHistory.Pop();
+                if (tmp.SequenceNumber == this.SequenceNumber)
+                {
+                    if (mHistory.Count == 0)
+                        return false;
+                    tmp = mHistory.Pop();
+                }
+
+                this.mRec = tmp.mRec;
+                this.FillColor = tmp.FillColor;
+                this.Thickness = tmp.Thickness;
+                this.LineColor = tmp.LineColor;
+                this.HasLine = tmp.HasLine;
+                this.HasFill = tmp.HasFill;
+                this.ListIndex = tmp.ListIndex;
+
+                return true;
+            }
+            return false;
+        }
+
+        public override bool IsMyRange(Point selectedPoint)
+        {
+            if (WIUtility.InRectanglePt(mRec, selectedPoint.X, selectedPoint.Y))
+                return true;
+            else
+                return false;
+
+        }
+    }
+
+    public class CTriagle : Shape
+    {
+        private Stack<CTriagle> mHistory;
+        private Rectangle mRec;
+        private int baseListIndex;
+
+        public CTriagle(Rectangle recParam)
+        {
+            mHistory = new Stack<CTriagle>();
+            mRec = new Rectangle();
+            mRec = recParam;
+            baseListIndex = 0;
+        }
+
+        public int ListIndex
+        {
+            set { baseListIndex = value; }
+            get { return baseListIndex; }
+        }
+
+        public override int GetStackCount()
+        {
+            return mHistory.Count;
+        }
+
+        public override void Draw(Graphics g)
+        {
+
+            PointF point1 = new PointF(mRec.X + ((mRec.Width)/2), mRec.Y);
+            PointF point2 = new PointF(mRec.X, mRec.Y + mRec.Height);
+            PointF point3 = new PointF(mRec.Width + mRec.X, mRec.Y + mRec.Height);
+            PointF[] curvePoints = { point1, point2, point3 };
+
+            if (base.HasFill)
+            {
+                SolidBrush brush = new SolidBrush(base.FillColor);
+                g.FillPolygon(brush, curvePoints);
+            }
+            if (base.HasLine)
+            {
+                Pen pen = new Pen(base.LineColor, base.Thickness);
+                g.DrawPolygon(pen, curvePoints);
+            }
+        }
+
+        public override Rectangle GetRenctangle()
+        {
+            return mRec;
+        }
+
+        public override void SetRenctangle(Rectangle recParam)
+        {
+            mRec = recParam;
+        }
+
+        public override void Save()
+        {
+            CTriagle tmp = new CTriagle(this.mRec);
+            tmp.baseListIndex = this.baseListIndex;
+            tmp.Thickness = this.Thickness;
+            tmp.LineColor = this.LineColor;
+            tmp.FillColor = this.FillColor;
+            tmp.HasLine = this.HasLine;
+            tmp.HasFill = this.HasFill;
+            this.SequenceNumber++;
+            tmp.SequenceNumber = this.SequenceNumber;
+
+            mHistory.Push(tmp);
+        }
+
+
+        public override bool Undo() // 실행 취소
+        {
+            if (mHistory.Count > 0)
+            {
+                CTriagle tmp = mHistory.Pop();
                 if (tmp.SequenceNumber == this.SequenceNumber)
                 {
                     if (mHistory.Count == 0)
